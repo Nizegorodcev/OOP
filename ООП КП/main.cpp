9b457c0c61709f1bfba83c15aaba64bc397d2f96
Nizegorodcev <113534451+Nizegorodcev@users.noreply.github.com>
2023-05-06 13:44:33 +0300

    Update main.cpp

--- ООП КП/main.cpp
+++ ООП КП/main.cpp
@@ -2,14 +2,119 @@
 #include "Taxi.h"
 #include<Windows.h>
 #include<fstream>
-void writefile(Taxi* arr,string path)//Запись в файл
+#include<iomanip>
+#include <algorithm>
+
+//Taxi* sort(Taxi* arr,int size) {
+//	Taxi* arr1 = new Taxi[1000];
+//	return arr1;
+//}
+int del(Taxi* arr, int size,int index) {
+	for (int i = index; i < size - 1; i++)
+	{
+		arr[i].client = arr[i + 1].client;
+		arr[i].where_from = arr[i + 1].where_from;
+		arr[i].where = arr[i + 1].where;
+		arr[i].driver = arr[i + 1].driver;
+		arr[i].colour = arr[i + 1].colour;
+		arr[i].marka = arr[i + 1].marka;
+		arr[i].registration_mark = arr[i + 1].registration_mark;
+		arr[i].distance = arr[i + 1].distance;
+		arr[i].price = arr[i + 1].price;
+	}
+	size--;
+	return size;
+}
+int comparator(string v1,string v2)
+{
+	if (v1.compare(v2) > 0) return 1;
+	else return -1;
+}
+void printmas(Taxi* arr, int size) {//Печать массива
+	cout << "_________________________________________________________________________________________________________________________________________________________________" << endl;
+	for (int i = 0; i < size; i++) {
+
+		cout << "|" << arr[i].client << setw(40 - arr[i].client.length()) << "|" << arr[i].where_from << setw(40 - arr[i].where_from.length()) << "|" << arr[i].where << setw(40 - arr[i].where.length()) << "|" << arr[i].driver << setw(40 - arr[i].driver.length()) << "|" << "\n";
+		cout << "-----------------------------------------------------------------------------------------------------------------------------------------------------------------" << endl;
+	}
+}
+int del(Taxi* arr,int size)//Удаление записи
+{
+
+	int fiodr, otkyda, kyda, fiocl;
+	int index;
+	string cl, ot, ky, dr;
+	cl = "ФИО клиента";
+	ot = "Откуда";
+	ky = "Куда";
+	dr = "ФИО водителя";
+	fiocl = 40 - cl.length();
+	otkyda = 40 - ot.length();
+	kyda = 40 - ky.length();
+	fiodr = 40 - dr.length();
+	cout << "__________________________________________________________________________________________________________________________________________________________________" << endl;
+	cout << " |" << cl << setw(fiocl) << "|" << ot << setw(otkyda) << "|" << ky << setw(kyda) << "|" << dr << setw(fiodr) << "|" << endl;
+	for (int i = 0; i < size; i++) {
+		
+		cout <<i<<" |" << arr[i].client << setw(40 - arr[i].client.length()) << "|" << arr[i].where_from << setw(40 - arr[i].where_from.length()) << "|" << arr[i].where << setw(40 - arr[i].where.length()) << "|" << arr[i].driver << setw(40 - arr[i].driver.length()) << "|" << "\n";
+		cout << "------------------------------------------------------------------------------------------------------------------------------------------------------------------" << endl;
+	}
+	cout << "Какую строку вы хотите удалить?" << endl;
+	cin >> index;
+	if (index > size)
+	{
+		cout << "Такого элемента  не существует" << endl;
+	}
+	else
+	{
+		
+		for (int i = index; i < size-1; i++)
+		{
+			arr[i].client = arr[i + 1].client;
+			arr[i].where_from = arr[i + 1].where_from;
+			arr[i].where = arr[i + 1].where;
+			arr[i].driver = arr[i + 1].driver;
+			arr[i].colour = arr[i + 1].colour;
+			arr[i].marka = arr[i + 1].marka;
+			arr[i].registration_mark = arr[i + 1].registration_mark;
+			arr[i].distance = arr[i + 1].distance;
+			arr[i].price = arr[i + 1].price;
+		}
+		size--;
+		cout << "_________________________________________________________________________________________________________________________________________________________________" << endl;
+		cout << "|" << cl << setw(fiocl) << "|" << ot << setw(otkyda) << "|" << ky << setw(kyda) << "|" << dr << setw(fiodr) << "|" << endl;
+		for (int i = 0; i < size; i++) {
+
+			cout <<i<<" |" << arr[i].client << setw(40 - arr[i].client.length()) << "|" << arr[i].where_from << setw(40 - arr[i].where_from.length()) << "|" << arr[i].where << setw(40 - arr[i].where.length()) << "|" << arr[i].driver << setw(40 - arr[i].driver.length()) << "|" << "\n";
+			cout << "-----------------------------------------------------------------------------------------------------------------------------------------------------------------" << endl;
+		}
+		
+		return size;
+	}
+	
+}
+void writefile(Taxi* arr,string path,int size)//Запись в файл
 {
 	ofstream fout;
+	int fiodr, otkyda, kyda, fiocl;
+	fiocl = 10;
+	otkyda = 17;
+	kyda = 12;
+	fiodr = 25;
+	string cl, ot, ky, dr;
+	cl = "ФИО клиента";
+	ot = "Откуда";
+	ky = "Куда";
+	dr = "ФИО водителя";
+	fiocl = fiocl - cl.length() + 1;
+	otkyda = otkyda + 1 - ot.length();
+	kyda = kyda + 5 - ky.length();
+	fiodr = fiodr + 4 - dr.length();
 	string client, where_from, where, driver;
-	fout.open(path);
+	fout.open(path,ofstream::app);
 	if (fout.is_open())
 	{
-		cout << "Файл открыт." << endl;
+		/*cout << "Файл открыт." << endl;
 		cout << "Введите ФИО клиента:" << endl;
 		cin >> client;
 		cout << "Введите адрес откуда его забрали: " << endl;
@@ -18,7 +123,11 @@ void writefile(Taxi* arr,string path)//Запись в файл
 		cin >> where;
 		cout << "Введите ФИО его водителя:" << endl;
 		cin >> driver;
-		fout << client<<"\t" << where_from<<"\t" << where <<"\t" << driver << endl;
+		fout <<"|" << client << setw(40 - client.length()) << "|" << where_from << setw(40 - where_from.length()) << "|" << where << setw(40 - where.length()) << "|" << driver << setw(40 - driver.length()) << "|" << "\n";*/
+		for (int i = 0; i < size; i++)
+		{
+			fout << "|" << arr[i].client << setw(40 - arr[i].client.length()) << "|" << arr[i].where_from << setw(40 - arr[i].where_from.length()) << "|" << arr[i].where << setw(40 - arr[i].where.length()) << "|" << arr[i].driver << setw(40 - arr[i].driver.length()) << "|" << "\n";
+		}
 	}
 	else
 	{
@@ -26,20 +135,35 @@ void writefile(Taxi* arr,string path)//Запись в файл
 	}
 	fout.close();
 }
-void readfile(string path)
+void readfile(string path)//Чтение
 {
 	ifstream fin;
 	string str;
+	int fiodr, otkyda, kyda, fiocl;
+	string cl, ot, ky, dr;
+	cl = "ФИО клиента";
+	ot = "Откуда";
+	ky = "Куда";
+	dr = "ФИО водителя";
+	fiocl = 40 - cl.length();
+	otkyda =  40 - ot.length();
+	kyda =  40 - ky.length();
+	fiodr =  40 - dr.length();
 	fin.open(path);
 	if ((fin.is_open()))
 	{	
 		cout << "Файл открыт." << endl;
-		cout <<"|    ФИО клиента   |Откуда|Куда |ФИО водителя                   |" << endl;
+		cout << "_________________________________________________________________________________________________________________________________________________________________" << endl;
+		cout <<"|"<<cl<< setw(fiocl)<<"|"<< ot <<setw(otkyda)<<"|"<<ky<<setw(kyda)<<"|"<<dr<<setw(fiodr)<<"|" << endl;
+		
+		/*printf("|%20S|%21S|%21S|%24S|\n", fiocl, otkyda, kyda, fiodr);*/
 		while (!fin.eof())
 		{
 			str = "";
+			cout << "-----------------------------------------------------------------------------------------------------------------------------------------------------------------" << endl;
 			getline(fin, str);
 			cout << str << endl;
+			
 		}
 	}
 	else
@@ -48,7 +172,7 @@ void readfile(string path)
 	}
 	fin.close();
 }
-bool compare(string a, string b)//Сравнение ФИО
+bool compare(string a, string b)//Сравнение ФИО водителя
 {
 	int kol = 0;
 	if (a.length() == b.length())
@@ -66,11 +190,12 @@ bool compare(string a, string b)//Сравнение ФИО
 	}
 	else return false;
 }
-void searchDriver(Driver* arr, int size)
+void searchDriver(Driver* arr, int size)//Функция поиска водителя по ФИО
 {
 	string value;
-	cout << "Введите ФИО водителя, которого вы хотите найти?" << endl;
+	cout << "Введите ФИО водителя, которого вы хотите найти:" << endl;
 	cin >> value;
+	int kol = 0;
 	for (int i = 0; i < size; i++)
 	{
 		if (compare(value, arr[i].driver) == true)
@@ -80,7 +205,9 @@ void searchDriver(Driver* arr, int size)
 			cout << "Цвет машины: " << arr[i].colour << endl;
 			cout << "Регистрационный знак: " << arr[i].registration_mark << endl;
 		}
+		else kol++;
 	}
+	if (kol == size) cout << "Такого в нашей базе данных нет" << endl;
 }
 void main()
 {
@@ -88,9 +215,9 @@ void main()
 	SetConsoleOutputCP(1251);
 	system("chcp 1251");
 
-	int len = 0;
+	int len = 0;//Длина массива водителей
 	int len1 = 0;
-	int len2 = 0;
+	int len2 = 0;//Для массива такси
 	int v;
 	string str;
 	Driver* a = new Driver [1000];//Будет содержать информацию о всех водителях и при заказе случайно направлять одного из них
@@ -110,7 +237,6 @@ void main()
 	a[2].registration_mark = "А228АА";
 	len += 3;
 	int size;
-	//client* arr2 = new client[1000];//Будет содержать информацию о клиентах
 	cout << "\t Вас приветствует Воронеж-такси!\n" << endl;
 	while (true)
 	{
@@ -119,7 +245,10 @@ void main()
 		cout << "2)Оформить заказ" << endl;
 		cout << "3)Поиск информации по ФИО водителя" << endl;
 		cout << "4)Считать и вывести данные с файла" << endl;
-		cout << "5)Записать данные в файл." << endl;//В разработке
+		cout << "5)Записать данные массива в файл." << endl;//В разработке
+		cout << "6)Удаление записи массива" << endl;
+		cout << "7)Печать массива" << endl;
+		cout << "8)Сортировка массива" << endl;
 		cout << "0)Выход" << endl;
 		cin >> v;
 		switch (v)
@@ -132,8 +261,12 @@ void main()
 		case 2:
 			arr->order(arr, len2);
 			arr->getTaxi(arr, len2);
+			len1 = a->getdriver(a, len);
+			arr[len2].driver = a[len1].driver;
+			arr[len2].colour = a[len1].colour;
+			arr[len2].marka = a[len1].marka;
+			arr[len2].registration_mark = a[len1].registration_mark;
 			len2++;
-			arr->getdriver(a, len);
 			break;
 		case 3:
 			searchDriver(a, len);
@@ -147,18 +280,50 @@ void main()
 		case 5:
 			cout << "Введите название файла в который вы хотите записать данные:" << endl;
 			cin >> path;
-			writefile(arr,path);
+			writefile(arr,path,len2);
 
 			break;
+		case 6:
+			len2= del(arr, len2);
+			break;
+		case 7:
+			printmas(arr, len2);
+			break;
+		case 8://Ищем минимальный элемент и записываем во 2 массив, в 1 массиве удаляем и делаем это снова
+			int index;
+			int lenpr = len2;//Копия длины массива, чтобы ее уменьшать
+			string min=arr[0].client;
+			Taxi* arr1 = new Taxi[1000];
+			for (int i = 0; i < len2; i++) {
+				for (int j = 1; j < lenpr; j++) {
+					/*qsort(arr, len2, sizeof(Taxi*), comparator);*/// Не работает
+					if (comparator(min, arr[j].driver) > 0) {
+						min = arr[j].driver;
+						index = j;
+					}
+				}
+				arr1[i].client = arr[index].client;
+				arr1[i].where_from = arr[index].where_from;
+				arr1[i].where = arr[index].where;
+				arr1[i].driver = arr[index].driver;
+				arr1[i].colour = arr[index].colour;
+				arr1[i].marka = arr[index].marka;
+				arr1[i].registration_mark = arr[index].registration_mark;
+				arr1[i].distance = arr[index].distance;
+				arr1[i].price = arr[index].price;
+				lenpr=del(arr, lenpr, index);
+			}
+			cout << "Отсортированный массив:" << endl;
+			printmas(arr1, len2);
+			
+			break;
 		}
+
 		if (v == 0)
 		{
 			delete[] arr;
 			delete[] a;
 			break;
 		}
-		
-		
-
 	}
 }
